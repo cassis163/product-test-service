3e218d76b3fd8587cc57a28e01b40843e3991a11
casper.aangeenbrug <casper.aangeenbrug@omoda.nl>
2022-05-23 12:06:30 +0200

    Add configuration for JPA repostories

--- src/main/java/nl/omoda/productestservice/ProductestserviceApplication.java
+++ src/main/java/nl/omoda/productestservice/ProductestserviceApplication.java
@@ -2,8 +2,10 @@ package nl.omoda.productestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ProductestserviceApplication {
 
 	public static void main(String[] args) {
