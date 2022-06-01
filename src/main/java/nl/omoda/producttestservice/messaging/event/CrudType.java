a928607b9d5fbf5cec10f4af5f7ca4bab5494550
casper.aangeenbrug <casper.aangeenbrug@omoda.nl>
2022-06-01 16:28:00 +0200

    Fix typo

--- src/main/java/nl/omoda/producttestservice/messaging/event/CrudType.java
+++ src/main/java/nl/omoda/producttestservice/messaging/event/CrudType.java
@@ -2,7 +2,7 @@ package nl.omoda.producttestservice.messaging.event;
 
 public abstract class CrudType {
     public static final String CREATE = "Create";
-    public static final String READ = "READ";
+    public static final String READ = "Read";
     public static final String UPDATE = "Update";
-    public static final String Delete = "Delete";
+    public static final String DELETE = "Delete";
 }
